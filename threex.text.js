43404f7d114aa0ad3cebd311982b93388e064d9a
Jerome Etienne <jerome.etienne@gmail.com>
2014-02-25 06:39:34 +0100

    more work

--- threex.text.js
+++ threex.text.js
@@ -17,7 +17,7 @@ THREEx.Text	= function(text, options){
 	geometry.computeBoundingBox();
 	var center	= new THREE.Vector3();
 	center.x	= (geometry.boundingBox.max.x - geometry.boundingBox.min.x) / 2
-	center.y	= (geometry.boundingBox.max.y - geometry.boundingBox.min.y) / 2
+	// center.y	= (geometry.boundingBox.max.y - geometry.boundingBox.min.y) / 2
 	center.z	= (geometry.boundingBox.max.z - geometry.boundingBox.min.z) / 2
 	geometry.vertices.forEach(function(vertex){
 		vertex.sub(center)
